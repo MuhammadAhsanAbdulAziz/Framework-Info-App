d80fd758b0ba578658e102b50aa391db0e328f52
Muhammad Ahsan bin Abdul Aziz <maahsan36@gmail.com>
2023-11-17 16:25:36 +0500

    Initial Commit

---
+++ Framework-Info-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Framework-Info-App
+//
+//  Created by Macbook Pro on 17/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Framework-Info-App/Framework_Info_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Framework_Info_AppApp.swift
+//  Framework-Info-App
+//
+//  Created by Macbook Pro on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Framework_Info_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
